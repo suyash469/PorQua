5868e1370f4bade97af6abd66b1598d76b1f1d24
suyash469 <suyashgupta6385@gmail.com>
2026-02-03 23:57:28 +0530

    feat: Improve data loading and add unit tests for data_loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -7,8 +7,6 @@ Copyright (c) 2024 Cyril Bachelard
 Licensed under GNU LGPL.3, see LICENCE file
 '''
 
-
-
 import os
 from typing import Optional, Union, Any
 import pandas as pd
@@ -35,24 +33,28 @@ def load_data_msci(path: str = None, n: int = 24) -> dict[str, pd.DataFrame]:
     '''Loads MSCI daily returns data from 1999-01-01 to 2023-04-18'''
 
     path = os.path.join(os.getcwd(), f'data{os.sep}') if path is None else path
-    # Load msci country index return series
+    
+    # --- FILE 1: MSCI Country Indices ---
     df = pd.read_csv(os.path.join(path, 'msci_country_indices.csv'),
-                        sep=';',
+                        sep=',',              # FIXED: Separator is comma
                         index_col=0,
                         header=0,
                         parse_dates=True)
-    df.index = pd.to_datetime(df.index, format='%d/%m/%Y')
+    
+    # FIXED: Date format uses dashes
+    df.index = pd.to_datetime(df.index, format='%d-%m-%Y')
+    
     series_id = df.columns[0:n]
     X = df[series_id]
 
-    # Load msci world index return series
+    # --- FILE 2: World Index (NDDLWI) ---
     y = pd.read_csv(f'{path}NDDLWI.csv',
-                    sep=';',
+                    sep=',',
                     index_col=0,
                     header=0,
                     parse_dates=True)
 
-    y.index = pd.to_datetime(y.index, format='%d/%m/%Y')
+    # FIXED: Date format uses dashes here too (Line 55 fixed)
+    y.index = pd.to_datetime(y.index, format='%d-%m-%Y')
 
     return {'return_series': X, 'bm_series': y}
-
